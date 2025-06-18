891ad4df64710d816c4cf9219e11ba6ea6a41dc0
BRIAN GITHINJI <brian.githinji3@student.moringaschool.com>
2025-06-18 01:28:33 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+// Write your code below!
+function scuberGreetingForFeet(feet) { // Write your code here!
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (2000 < feet && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+}
+function ternaryCheckCity(city) {
   // Write your code here!
+ return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  // Note: 'city' should be defined in the context where this function is called.
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+ }
